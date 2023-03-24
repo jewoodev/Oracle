4797999d80ae426512c05305930980dd339134fd
jewoodev <jewoos15@naver.com>
2023-03-25 00:09:31 +0900

    Try drawing ERD

--- PRECEDURE.sql
+++ PRECEDURE.sql
@@ -1,17 +1,26 @@
-CREATE VIEW vw_Book
-AS SELECT *
-FROM BOOK
-WHERE BOOKNAME LIKE '%축구%';
+--프로시저(실행부)
+EXEC INSERTBOOK(13, '스포츠과학', '마당과학서적', 25000);
+--확인 작업
+SELECT * FROM BOOK;
 
-CREATE OR REPLACE VIEW vw_Customer
-AS SELECT *
-FROM Customer
-WHERE address LIKE '%대한민국%';
+/* BookInsertOrUpdate 프로시저를 실행해서 테스트하는 부분*/
+--1
+EXEC BookInsertOrUpdate(15, '스포츠 즐거움', '마당과학서적', 25000);
+SELECT *FROM Book;
+--2
+EXEC BookInsertOrUpdate(15, '스포츠 즐거움', '마당과학서적', 20000);
+SELECT *FROM Book;
 
-CREATE OR REPLACE VIEW vw_Customer(custid, name, address)
-AS SELECT custid, name, address
-FROM Customer
-WHERE address LIKE '%영국%';
+/*프로시저 AveragePrice를 테스트하는 부분*/
+SET SERVEROUTPUT ON;
+DECLARE
+    AverageVal NUMBER;
+BEGIN
+    AveragePrice(AverageVal);
+    DBMS_OUTPUT.PUT_LINE('책값 평균: '|| AverageVal);
+END;
 
-SELECT *
-FROM vw_Customer;
+--커서를 사용하는 프로시저
+ /* Interest 프로시저를 실행하여 판매된 도서에 대한 이익금을 계산 */
+SET SERVEROUTPUT ON;
+EXEC Interest;
